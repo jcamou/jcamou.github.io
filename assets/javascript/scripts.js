6d4ebe0dde231153d0b8905b371881c8c7d445dd
jcamou <jccamou@gmail.com>
2018-02-14 15:12:30 -0500

    [ADD] Add jquery functionality to mobile nav

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  $("#js-site-nav__menu").click(function() {
-    $("#js-site-nav__list--mobile").toggleClass("is-open");
-    $("#js-site-nav__list--mobile").fadeIn("slow");
+  $(".js-site-nav__toggle").click(function() {
+    $(".js-site-nav__list").toggleClass("is-open");
   });
 });
